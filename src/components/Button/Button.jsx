38a113da5b6620ebcc0b6c1f70313ae377a65995
Vovcharenko Anna <129333985+AnnaVovcharenko@users.noreply.github.com>
2023-10-19 23:51:51 +0300

    end

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ButtonLoad } from './Button.styled';
 
 export const Button = ({onClick}) => {
+    
     return(
         <div>
             <ButtonLoad type="button" onClick={onClick}>
